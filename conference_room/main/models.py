8111e3c584332d1c0377e3d1158af8008906e777
Shizol01 <wrzosek-krystian@wp.pl>
2025-04-21 14:43:02 +0200

    Created models for room and reservation. Created base.html in templates.

--- conference_room/main/models.py
+++ conference_room/main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Room(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    capacity = models.PositiveIntegerField()
+    projector = models.BooleanField()
+
+    def __str__(self):
+        return self.name
+
+class Reservation(models.Model):
+    date = models.DateField()
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='reservations')
+    comment = models.TextField()
+
+    class Meta:
+        unique_together = ('date', 'room_id')
+
+    def __str__(self):
+        return f"{self.room.name} - {self.date}"
